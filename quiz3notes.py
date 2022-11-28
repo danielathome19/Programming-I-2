bfa8aadc05e935c8f610cac062d74907bb48b5b6
Daniel J. Szelogowski <dszelogowski@gmail.com>
2022-11-28 15:07:22 +0000

    Update

--- quiz3notes.py
+++ quiz3notes.py
@@ -2,8 +2,12 @@ text = input("Enter text: ")
 for index in range(len(text)):
   print(text[index], end=" ")
 print()
+numwords = 0
 for letter in text:  # for-each loop
+  if letter == " ":
+    numwords = numwords + 1
   print(letter)
+print("The number of words is", numwords + 1)
 
 def main():
   text = "cool beans"
